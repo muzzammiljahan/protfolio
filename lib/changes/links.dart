18f02c167126eed9cd4c332062bcc49d8dbf556b
muzzammil.ma@wizklub.com <muzzammil.ma@wizklub.com>
2023-02-25 11:10:33 +0530

    first commit

---
+++ lib/changes/links.dart
@@ -0,0 +1,37 @@
+// CV
+const String resume =
+    'https://drive.google.com/file/d/1LanvFQMHWAdQZdKKMGaFIXD3I-rdjndj/view?usp=sharing';
+
+// Github Link
+const String gitHub = 'https://github.com/muzzammiljahan';
+// Github Link
+const String facebook = 'https://facebook.com/';
+// Github Link
+const String twitter = 'https://twitter.com/';
+// Github Link
+const String youtube = 'https://www.youtube.com/channel/';
+// Github Link
+const String linkedin =
+    'https://www.linkedin.com/public-profile/settings?trk=d_flagship3_profile_self_view_public_profile';
+// Github Link
+const String buymeacoffee = 'https://protfolio.muhammedmuzzammil.com/#/';
+// Github Link
+const String instagram = 'https://instagram.com/';
+// socialLinks
+// const List<String> socialLinks = [
+//   "https://facebook.com/sudeshnb",
+//   // "https://instagram.com/sudesh_nish",
+//   "https://www.youtube.com/channel/UCXooUY2oL_eqGhTaZn-ExSg",
+//   "https://twitter.com/sudesh78",
+//   "https://linkedin.com/in/sudeshnb",
+//   "https://www.buymeacoffee.com/sudeshnb", "https://github.com/sudeshnb",
+// ];
+//
+const String profileImage = "https://protfolio.muhammedmuzzammil.com/#/";
+//
+// Github Link
+const String fiverr = 'https://protfolio.muhammedmuzzammil.com/#/';
+// Github Link
+const String upwork = 'https://protfolio.muhammedmuzzammil.com/#/';
+// Github Link
+const String whatsapp = 'https://api.whatsapp.com/send?phone=9535346318';
